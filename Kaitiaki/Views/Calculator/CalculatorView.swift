15f15ff8f4eee49fe0a324a8c5e479abbc636612
boubelpi <113375173+boubelpi@users.noreply.github.com>
2023-06-05 10:53:17 +0200

    CalculatorView added

---
+++ Kaitiaki/Views/Calculator/CalculatorView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorView.swift
+//  Kaitiaki
+//
+//  Created by Pavel Boubel on 05/06/23.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text("There will be calculator")
+    }
+}
+
+struct CalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView()
+    }
+}

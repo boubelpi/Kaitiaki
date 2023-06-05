ee691aec7b4d706e44aba7d3077e737371acfa95
boubelpi <113375173+boubelpi@users.noreply.github.com>
2023-06-05 12:24:57 +0200

    Calculator added

--- Kaitiaki/TabBarView.swift
+++ Kaitiaki/TabBarView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct TabBarView: View {
     var body: some View {
         TabView {
-            CalculatorView()
+            CalculatorView(txt : "")
                 .tabItem {
                     Label("Carbon footprint calculator", systemImage: "leaf.fill")
                 }
